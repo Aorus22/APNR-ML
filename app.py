90436776d4803047a0e0fa70735d8adfd15064cc
aorus22 <alyzanadif04@gmail.com>
2024-12-09 14:15:29 +0700

    update best.pt

--- app.py
+++ app.py
@@ -96,6 +96,9 @@ def predict():
     response = {'plates': plates_info}
     return jsonify(response)
 
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
